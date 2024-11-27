af513d093957955eeaf7d05abc2f2d8e5b653e71
alu0101205953 <alu0101205953@ull.edu.es>
2024-11-27 15:43:45 +0000

    Added menu pdf

--- src/components/DownloadMenu.jsx
+++ src/components/DownloadMenu.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
-import './DownloadMenu.css'; // Asegúrate de importar el archivo CSS
+import './DownloadMenu.css'; 
 
 const CartaDownload = () => {
   useEffect(() => {
     const link = document.createElement('a');
-    link.href = 'https://drive.google.com/uc?export=download&id=1A2B3C4D5E6F7G8H9'; // Reemplaza con tu ID de Google Drive
-    link.download = 'menu.pdf'; // Nombre del archivo descargado
+    link.href = 'https://drive.google.com/uc?export=download&id=1ekdUnthFI5YBke0bl2rGdIuTH3XNK6Ka'; // Reemplaza con tu ID de Google Drive
+    link.download = 'menu-cinnamon-atelier.pdf'; // Nombre del archivo descargado
     link.click();
   }, []);
 
@@ -16,7 +16,7 @@ const CartaDownload = () => {
         <p>Tu descarga debería comenzar automáticamente. Si no es así, haz clic en el botón de abajo.</p>
       </div>
       <div>
-        <a href="https://drive.google.com/uc?export=download&id=1A2B3C4D5E6F7G8H9" className="download-button">
+        <a href="https://drive.google.com/uc?export=download&id=1ekdUnthFI5YBke0bl2rGdIuTH3XNK6Ka" className="download-button">
           Descargar Menú
         </a>
       </div>
